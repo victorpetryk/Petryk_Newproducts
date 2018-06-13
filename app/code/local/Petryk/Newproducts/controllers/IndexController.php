e188e8ae4a3a1c75035da094a93533520b8c7ccb
Victor Petryk <victor.petryk@gmail.com>
2018-06-13 12:12:42 +0300

    Створено контроллер та налаштовано маршрут

---
+++ app/code/local/Petryk/Newproducts/controllers/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+
+class Petryk_Newproducts_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function listAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+}
